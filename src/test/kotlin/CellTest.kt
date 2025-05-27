03a56bdc116689def3540286f55b8f2b5dc48dbf
Yuria Loo <129998654+yuria-loo@users.noreply.github.com>
2025-05-27 16:58:32 -0700

    Add a templete for tests

---
+++ src/test/kotlin/CellTest.kt
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CellTest {
+
+    @Test
+    fun getModel() {
+    }
+
+    @Test
+    fun getLaunchAnnounced() {
+    }
+
+    @Test
+    fun getLaunchStatus() {
+    }
+
+    @Test
+    fun getBodyDimensions() {
+    }
+
+    @Test
+    fun getBodyWeight() {
+    }
+
+    @Test
+    fun getBodySim() {
+    }
+
+    @Test
+    fun getDisplayType() {
+    }
+
+    @Test
+    fun getDisplaySize() {
+    }
+
+    @Test
+    fun getDisplayResolution() {
+    }
+
+    @Test
+    fun getFeatSensors() {
+    }
+
+    @Test
+    fun getPlatformOs() {
+    }
+
+    @Test
+    fun extractYear() {
+    }
+
+    @Test
+    fun extractReleaseYear() {
+    }
+}
